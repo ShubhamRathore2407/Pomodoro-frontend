b052d692af8e640492d48594825449c9df98345e
Shubham Rathore <shubhamrathore@memorres.com>
2023-12-18 19:20:00 +0530

    update4

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import LoginSignup from './pages/LoginSignup';
-import axios from 'axios';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginSignup />} />
-        <Route path="/signup" element={<LoginSignup />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginSignup />} />
+        <Route path='/signup' element={<LoginSignup />} />
       </Routes>
     </Router>
   );
